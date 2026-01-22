5d421e5e9895fc7c8872c0c0347d6bd8aa6ea471
Victor Leveneur <victor.leveneur@proton.me>
2026-01-22 18:47:53 +0100

    Removed link between portfolio and user in db

--- backend/python/src/infrastructure/datastore/sqlalchemy/models/portfolio.py
+++ backend/python/src/infrastructure/datastore/sqlalchemy/models/portfolio.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from datetime import datetime
 import uuid
-from sqlalchemy import DateTime, ForeignKey, Integer, String, func
+from sqlalchemy import DateTime, Integer, String, UUID, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .asset import Asset
-    from .user import User
 
 from ..base import Base
 
@@ -17,7 +16,7 @@ class Portfolio(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
-    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False)
+    owner_id: Mapped[uuid.UUID] = mapped_column(UUID, index=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -29,5 +28,3 @@ class Portfolio(Base):
         cascade="all, delete-orphan", # delete assets when portfolio is deleted
         lazy="selectin", # optimize loading with selectin loading (avoids N+1 problem, loads assets in one query)
     )
-
-    owner: Mapped["User"] = relationship(back_populates="portfolios") # link back to User
--- backend/python/src/infrastructure/datastore/sqlalchemy/models/user.py
+++ backend/python/src/infrastructure/datastore/sqlalchemy/models/user.py
@@ -18,9 +18,3 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-
-    portfolios: Mapped[list["Portfolio"]] = relationship(
-        back_populates="owner",
-        cascade="all, delete-orphan",
-        lazy="selectin",
-    )
